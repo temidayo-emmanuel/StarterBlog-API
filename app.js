66ed4254ae3f8614a54996a6b056ef9cef91d0b2
temidayo <adebayotemidayo601@gmail.com>
2024-12-30 01:12:32 +0100

    made changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-    origin: ["http://localhost:5173", "https://starterblog-indol.vercel.app"],
+    origin: ["http://localhost:5173", "https://starterblog.onrender.com"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
